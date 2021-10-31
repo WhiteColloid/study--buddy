bab35877d286e9937643f52e26bac371a03318b2
WhiteColloid <yluo236@emory.edu>
2021-10-31 12:59:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import LoginForm from './components/LoginForm';
+import Header from './Header';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Student from './Student';
+import Professor from './Professor';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="page">
+        <div className="content">
+          <BrowserRouter>
+             <Header/>
+             <Route path='/' exact={true} component={Student}/>
+             <Route path='/private' exact={true} component={Professor}/>
+          </BrowserRouter>
+        </div>
+      </div>
     </div>
   );
 }
