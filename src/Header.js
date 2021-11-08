fccec9a821ea192a0936193ca2659d42ae4d2b2c
WhiteColloid <yluo236@emory.edu>
2021-11-07 21:34:52 -0500

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
+
 function Header() {
   return (
     <header>
-      <div>React Login</div>
+      <div>Welcome!</div>
       <ul className="menu">
         <li><Link to="/">Student</Link></li>
-        <li><Link to="/private">Professor</Link></li>
+        <li><Link to="/professor">Professor</Link></li>
+        <li><Link to="/quiz">Quiz</Link></li>
       </ul>
     </header>
   );
